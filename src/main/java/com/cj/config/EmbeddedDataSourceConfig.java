fad572aeb03b78fe97df1ccd2efe0e807beba898
YouBenshan <benshan.you@gmail.com>
2013-09-02 13:49:35 +0800

    forget to commit

--- src/main/java/com/cj/config/EmbeddedDataSourceConfig.java
+++ src/main/java/com/cj/config/EmbeddedDataSourceConfig.java
@@ -12,7 +12,7 @@ import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
-@Profile({"default",  "test" })
+@Profile({"test" })
 class EmbeddedDataSourceConfig {
 
 	@Autowired
