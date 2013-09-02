0b6e2d89386b026ee0eaca50a56893d8900b6c2c
rickyzeng <weifa163@163.com>
2013-09-02 18:14:14 +0800

    修改 发送牙刷靓照参与有奖活动”修改为“发送牙刷靓照赢大奖”

--- src/main/java/com/cj/lion/replyer/ActivityReplyer.java
+++ src/main/java/com/cj/lion/replyer/ActivityReplyer.java
@@ -29,7 +29,7 @@ public class ActivityReplyer implements Replyer {
 
 	private static final String TITLE = "请提交联系方式来抽奖";
 	private static final String DESCRIPTION = "我们将选出最美牙刷";
-	private static final String DESCRIPTION_START = "发送牙刷靓照参与有奖活动";
+	private static final String DESCRIPTION_START = "发送牙刷靓照赢大奖";
 	private static final String PIC_URL_START = "/resources/site/student/student.jpg";
 	private static final String PIC_URL = "/resources/site/student/studentInfo.jpg";
 	private static final String URL_START="/resources/site/brand4.html";
